453897c423d2e299a5c4d6409fcf0d66b8631c81
Colton <cjohansson@eagles.ewu.edu>
2018-10-28 19:36:36 -0700

    first commit

---
+++ WOMM.java
@@ -0,0 +1,14 @@
+/**
+ * TEAM: Worked on my machine...
+ * Colton Johansson
+ * Joseph Carlson
+ * Brandtly Strobeck
+ */
+public class WOMM {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Cheney");
+
+    }
+}
